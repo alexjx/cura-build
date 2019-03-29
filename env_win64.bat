8ebbcf4b871a82dd2afc96bb48f40786cb64cc75
Jia Xin <alexjx@gmail.com>
2019-03-29 10:09:18 +0800

    use local source tree

--- env_win64.bat
+++ env_win64.bat
@@ -1,5 +1,5 @@
-set PYTHONPATH=%cd%\inst\lib\python3.5\site-packages
-set PATH=%cd%\inst\bin;%PATH%
+set PYTHONPATH=D:\_3dprinting\cura-build\env-3.6\lib\site-packages
+set PATH=D:\_3dprinting\cura-build\env-3.6\bin;%PATH%
 set CFLAGS=-m64
 set CXXFLAGS=-m64
 set LDFLAGS=-m64
