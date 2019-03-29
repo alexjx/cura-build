d28d0532d1dbea3fb0f4ab5b87687c3a100c67ff
Jia Xin <alexjx@gmail.com>
2019-03-29 10:33:48 +0800

    update environment

--- env_win64.bat
+++ env_win64.bat
@@ -1,5 +1,5 @@
 set PYTHONPATH=D:\_3dprinting\cura-build\env-3.6\lib\site-packages
-set PATH=D:\_3dprinting\cura-build\env-3.6\bin;%PATH%
+set PATH=D:\_3dprinting\cura-build\env-3.6\bin;C:\Program Files (x86)\NSIS;%PATH%
 set CFLAGS=-m64
 set CXXFLAGS=-m64
 set LDFLAGS=-m64
